56e27f4eb20a45505b42b45259442700eb358627
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2016-12-27 23:22:10 +0100

    dynamically calculate how many random numbers to generate

--- voting_app/Main.elm
+++ voting_app/Main.elm
@@ -27,44 +27,48 @@ type Msg
     | RandomVotes (List Int)
 
 
+data : List Product
+data =
+    [ { id = 1
+      , title = "Yellow Pail"
+      , description = "On-demand sand castle construction expertise."
+      , url = "#"
+      , votes = 0
+      , submitter_avatar_url = "images/avatars/daniel.jpg"
+      , product_image_url = "images/products/image-aqua.png"
+      }
+    , { id = 2
+      , title = "Supermajority: The Fantasy Congress League"
+      , description = "Earn points when your favorite politicians pass legislation."
+      , url = "#"
+      , votes = 0
+      , submitter_avatar_url = "images/avatars/kristy.png"
+      , product_image_url = "images/products/image-rose.png"
+      }
+    , { id = 3
+      , title = "Tinfoild: Tailored tinfoil hats"
+      , description = "We already have your measurements and shipping address."
+      , url = "#"
+      , votes = 0
+      , submitter_avatar_url = "images/avatars/veronika.jpg"
+      , product_image_url = "images/products/image-steel.png"
+      }
+    , { id = 4
+      , title = "Haught or Naught"
+      , description = "High-minded or absent-minded? You decide."
+      , url = "#"
+      , votes = 0
+      , submitter_avatar_url = "images/avatars/molly.png"
+      , product_image_url = "images/products/image-yellow.png"
+      }
+    ]
+
+
 init : ( Model, Cmd Msg )
 init =
-    ( { products =
-            [ { id = 1
-              , title = "Yellow Pail"
-              , description = "On-demand sand castle construction expertise."
-              , url = "#"
-              , votes = 0
-              , submitter_avatar_url = "images/avatars/daniel.jpg"
-              , product_image_url = "images/products/image-aqua.png"
-              }
-            , { id = 2
-              , title = "Supermajority: The Fantasy Congress League"
-              , description = "Earn points when your favorite politicians pass legislation."
-              , url = "#"
-              , votes = 0
-              , submitter_avatar_url = "images/avatars/kristy.png"
-              , product_image_url = "images/products/image-rose.png"
-              }
-            , { id = 3
-              , title = "Tinfoild: Tailored tinfoil hats"
-              , description = "We already have your measurements and shipping address."
-              , url = "#"
-              , votes = 0
-              , submitter_avatar_url = "images/avatars/veronika.jpg"
-              , product_image_url = "images/products/image-steel.png"
-              }
-            , { id = 4
-              , title = "Haught or Naught"
-              , description = "High-minded or absent-minded? You decide."
-              , url = "#"
-              , votes = 0
-              , submitter_avatar_url = "images/avatars/molly.png"
-              , product_image_url = "images/products/image-yellow.png"
-              }
-            ]
+    ( { products = data
       }
-    , Random.generate RandomVotes (Random.list 4 (Random.int 15 65))
+    , Random.generate RandomVotes (Random.list (List.length data) (Random.int 15 65))
     )
 
 
