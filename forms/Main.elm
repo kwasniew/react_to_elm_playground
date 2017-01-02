fd69e3c00aed291011539384d5b1775543e7ec82
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2017-01-02 11:27:27 +0100

    init forms project

---
+++ forms/Main.elm
@@ -0,0 +1,34 @@
+module Main exposing (..)
+
+import Html exposing (..)
+
+
+type alias Model =
+    {}
+
+
+type Msg
+    = NoOp
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( {}, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ h1 [] [ text "Sign Up Sheet" ]
+        ]
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { view = view, init = init, update = update, subscriptions = (\_ -> Sub.none) }
