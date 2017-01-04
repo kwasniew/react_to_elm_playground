028247996f06b1193bc1801bafede040dc24c538
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2017-01-04 12:19:29 +0100

    truncate no op spec

--- testing/basics/Tests.elm
+++ testing/basics/Tests.elm
@@ -8,11 +8,20 @@ import Modash
 all : Test
 all =
     describe "Modash"
-        [ test "`truncate`: trauncates a string" <|
-            \() ->
-                let
-                    string =
-                        "there was one catch, and that was CATCH-22"
-                in
-                    Expect.equal (Modash.truncate string 19) "there was one catch..."
+        [ describe "`truncate:`"
+            [ test "truncates a string" <|
+                \() ->
+                    let
+                        string =
+                            "there was one catch, and that was CATCH-22"
+                    in
+                        Expect.equal (Modash.truncate string 19) "there was one catch..."
+            , test "no-ops if <= length" <|
+                \() ->
+                    let
+                        string =
+                            "there was one catch, and that was CATCH-22"
+                    in
+                        Expect.equal (Modash.truncate string (String.length string)) string
+            ]
         ]
