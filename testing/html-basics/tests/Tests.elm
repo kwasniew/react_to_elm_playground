72bcdf7d19d5baf0a09da7faa0850d0f2bcc8802
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2017-01-05 12:34:09 +0100

    disable attribute test

--- testing/html-basics/tests/Tests.elm
+++ testing/html-basics/tests/Tests.elm
@@ -3,12 +3,7 @@ module Tests exposing (suite)
 import Test exposing (test, Test, describe)
 import App exposing (view)
 import Test.Html.Query as Query
-import Test.Html.Selector exposing (text, tag, all, Selector)
-
-
-element : String -> String -> Selector
-element tagName txt =
-    all [ tag tagName, text txt ]
+import Test.Html.Selector exposing (tag, attribute, boolAttribute, text, all, Selector)
 
 
 html : Query.Single
@@ -22,11 +17,11 @@ suite =
         [ test "should have `th` \"Items\"" <|
             \() ->
                 html
-                    |> Query.has [ element "th" "Items" ]
+                    |> Query.has [ all [ tag "th", text "Items" ] ]
         , test "should have a `button` element" <|
             \() ->
                 html
-                    |> Query.has [ element "button" "Add item" ]
+                    |> Query.has [ all [ tag "button", boolAttribute "disabled" True, text "Add item" ] ]
         , test "should have an `input` element" <|
             \() ->
                 html |> Query.has [ tag "input" ]
