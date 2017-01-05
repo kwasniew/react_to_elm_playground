9382cd561e36e17a43cefb7319800fc7c4f36590
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2017-01-05 13:06:49 +0100

    tests for populating input

--- testing/html-basics/tests/Tests.elm
+++ testing/html-basics/tests/Tests.elm
@@ -1,14 +1,20 @@
 module Tests exposing (suite)
 
 import Test exposing (test, Test, describe)
-import App exposing (view)
+import App exposing (..)
 import Test.Html.Query as Query
 import Test.Html.Selector exposing (tag, attribute, boolAttribute, text, all, Selector)
+import Expect
 
 
-html : Query.Single
-html =
-    App.view { items = [], item = "" } |> Query.fromHtml
+html : Model -> Query.Single
+html model =
+    App.view model |> Query.fromHtml
+
+
+model : Model
+model =
+    { items = [], item = "" }
 
 
 suite : Test
@@ -16,13 +22,24 @@ suite =
     describe "App"
         [ test "should have `th` \"Items\"" <|
             \() ->
-                html
+                html model
                     |> Query.has [ all [ tag "th", text "Items" ] ]
         , test "should have a `button` element" <|
             \() ->
-                html
+                html model
                     |> Query.has [ all [ tag "button", boolAttribute "disabled" True, text "Add item" ] ]
         , test "should have an `input` element" <|
             \() ->
-                html |> Query.has [ tag "input" ]
+                html model |> Query.has [ tag "input" ]
+        , describe "the user populates the input"
+            [ test "should update the model property `item`" <|
+                \() ->
+                    Expect.equal
+                        (App.update (ItemChange "Vancouver") model)
+                        ( { items = [], item = "Vancouver" }, Cmd.none )
+            , test "should enable `button`" <|
+                \() ->
+                    html { items = [], item = "Vancouver" }
+                        |> Query.has [ all [ tag "button", boolAttribute "disabled" False ] ]
+            ]
         ]
