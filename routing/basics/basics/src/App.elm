d29d690c7e01ba655ca127919f4037b37bb6321d
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2017-01-06 17:15:55 +0100

    added initial html

--- routing/basics/basics/src/App.elm
+++ routing/basics/basics/src/App.elm
@@ -1,6 +1,7 @@
 module App exposing (..)
 
 import Html exposing (..)
+import Html.Attributes exposing (class, href)
 
 
 type alias Model =
@@ -16,9 +17,44 @@ init =
     ( {}, Cmd.none )
 
 
+atlantic : Html msg
+atlantic =
+    div []
+        [ h3 [] [ text "Atlantic Ocean" ]
+        , p [] [ text "The Atlantic Ocean covers approximately 1/5th of the surface of the earth. " ]
+        ]
+
+
+pacific : Html msg
+pacific =
+    div []
+        [ h3 [] [ text "Pacific Ocean" ]
+        , p [] [ text "Ferdinand Magellan, a Portuguese explorer, named the ocean 'mar pacifico' in 1521, which means peaceful sea. " ]
+        ]
+
+
 view : Model -> Html Msg
 view model =
-    div [] [ text "routing" ]
+    div [ class "ui text container" ]
+        [ h2 [ class "ui dividing header" ]
+            [ text "Which body of water?"
+            ]
+        , ul []
+            [ li []
+                [ a [ href "/atlantic" ]
+                    [ code []
+                        [ text "/atlantic" ]
+                    ]
+                ]
+            , li []
+                [ a [ href "/pacific" ]
+                    [ code []
+                        [ text "/pacific" ]
+                    ]
+                ]
+            ]
+        , hr [] []
+        ]
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
