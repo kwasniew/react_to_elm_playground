5cefd196b23f6b949681ceda473227e04aac3358
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2017-01-07 20:42:47 +0100

    added some styles from the react version

--- routing/music/client/src/static/index.js
+++ routing/music/client/src/static/index.js
@@ -1,5 +1,6 @@
 // pull in desired CSS/SASS files
 require('./semantic/dist/semantic.min.css');
+require('./styles.css');
 
 // inject bundled Elm app into div#main
 var Elm = require( '../elm/App' );
